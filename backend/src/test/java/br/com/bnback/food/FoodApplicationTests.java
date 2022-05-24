0606080fc35dd445a0dd70fc366a857017aef9e4
ElielNascimento <nascimentob.eliel@gmail.com>
2022-05-24 02:39:53 -0300

    parte 1 crud spring boot

---
+++ backend/src/test/java/br/com/bnback/food/FoodApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bnback.food;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
